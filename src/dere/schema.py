37792f3b95b441907523720307a27eaab5efabc1
Sean Papay <sean.papay@gmail.com>
2018-07-06 13:19:33 +0200

    Made the schema classes dataclasses

--- src/dere/schema.py
+++ src/dere/schema.py
@@ -1,45 +1,28 @@
 from typing import List, Optional, Union
+from dataclasses import dataclass
 
 
+@dataclass
 class SpanType:
-    def __init__(
-        self, name: str,
-        anchors: Optional[List[FrameType]] = None
-    ) -> None:
-        self.name = name
-        self.anchors = anchors
-
-    def __repr__(self) -> str:
-        return "<Span %s>" % self.name
+    name: str
+    anchors: Optional[List[FrameType]] = None
 
 
+@dataclass
 class FrameType:
-    def __init__(self, name: str, slots: List[SlotType]) -> None:
-        self.name = name
-        self.slots = list(slots)
-
-    def __repr__(self) -> str:
-        return "<Frame %s>" % self.name
+    name: str
+    slots: List[SlotType]
 
 
+@dataclass
 class SlotType:
-    def __init__(
-        self, name: str, types: List[Union[FrameType, SpanType]],
-        min_cardinality: int = 1, max_cardinality: int = 1
-    ) -> None:
-        self.name = name
-        self.types = types
-        self.min_cardinality = min_cardinality
-        self.max_cardinality = max_cardinality
-
-    def __repr__(self) -> str:
-        return "<Slot %s>" % self.name
+    name: str
+    types: List[Union[FrameType, SpanType]]
+    min_cardinality: int = 1
+    max_cardinality: int = 1
 
 
+@dataclass
 class TaskSchema:
-    def __init__(
-        self, span_types: List[SpanType],
-        frame_types: List[FrameType]
-    ) -> None:
-        self.span_types = list(span_types)
-        self.frame_types = list(frame_types)
+    span_types: List[SpanType]
+    frame_types: List[FrameType]
