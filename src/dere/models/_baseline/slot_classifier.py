59cd198ed1be9586d71b86438c6f4d3bbeabecde
Laura Bostan <sarnthil@gmail.com>
2018-07-31 22:24:36 +0200

    Fix slot_classifier (initialize CVs only while training)
    - Added one feature (path_deps; sequence words still missing)
    - General cleanup and YAGNIing

--- src/dere/models/_baseline/slot_classifier.py
+++ src/dere/models/_baseline/slot_classifier.py
@@ -1,8 +1,8 @@
 # Author: Laura
-import sys
-import pdb
 import copy
 import logging
+import random
+
 from itertools import chain, combinations, product
 from operator import mul
 from typing import Optional, Dict, Tuple, List, Set, Any
@@ -10,10 +10,9 @@ from typing import Optional, Dict, Tuple, List, Set, Any
 import networkx as nx
 import numpy as np
 import spacy
+
 from spacy.tokens import Doc
-from spacy.pipeline import DependencyParser
 from sklearn.svm import LinearSVC
-from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import (
     accuracy_score,
@@ -21,9 +20,7 @@ from sklearn.metrics import (
     confusion_matrix,
 )
 from sklearn.externals import joblib
-import random
-from scipy.sparse import hstack, csr_matrix
-import scipy
+from scipy.sparse import hstack, csr_matrix, spmatrix
 
 from dere import Result
 from dere.corpus import Corpus, Instance, Frame, Span, Filler
@@ -38,9 +35,9 @@ class SlotClassifier:
         self._spec = spec
         random.seed(98765)
         # Find our plausible relations from the spec
-        self.plausible_relations: Dict[
-            Tuple[SpanType, SpanType], List[Tuple[FrameType, SlotType]]
-        ] = {}
+        spanpair = Tuple[SpanType, SpanType]
+        edge = Tuple[FrameType, SlotType]
+        self.plausible_relations: Dict[spanpair, List[edge]] = {}
         labels: Set[Any] = {"Nothing"}
         # For every span type that triggers a frame
         for frame_type in spec.frame_types:
@@ -64,7 +61,7 @@ class SlotClassifier:
         )
 
     def train(self, corpus: Corpus, dev_corpus: Optional[Corpus] = None) -> None:
-        x, y = self.get_features_and_labels(corpus)
+        x, y = self.get_features_and_labels(corpus, is_train=True)
 
         if dev_corpus is None:
             self.cls = LinearSVC()
@@ -127,18 +124,19 @@ class SlotClassifier:
         return frame_type.slot_types[0]
 
     def get_features_and_labels(
-        self,
-        corpus: Corpus,
-    ) -> Tuple[scipy.sparse.spmatrix, np.ndarray]:
+        self, corpus: Corpus, is_train: bool = False
+    ) -> Tuple[spmatrix, np.ndarray]:
+
         labels: List[Any] = []
         span1_list: List[Span] = []
         span2_list: List[Span] = []
         doc_list: List[Doc] = []
         graph_list: List[nx.Graph] = []
         idx2word_list: List[Dict[int, str]] = []
+        edge2dep_list = []
         prev_status = -1
         for i, instance in enumerate(corpus.instances):
-            doc, graph, idx2word = self.preprocess_text(instance.text)
+            doc, graph, idx2word, edge2dep = self.preprocess_text(instance.text)
             span_pairs = self.get_span_pairs(instance)
             for span1, span2, relation in span_pairs:
                 span1_list.append(span1)
@@ -146,6 +144,7 @@ class SlotClassifier:
                 doc_list.append(doc)
                 graph_list.append(graph)
                 idx2word_list.append(idx2word)
+                edge2dep_list.append(edge2dep)
                 labels.append(relation)
             status = int(100 * i / len(corpus.instances))
             if prev_status != status and status % 10 == 0:
@@ -158,13 +157,19 @@ class SlotClassifier:
         # (list(iwl.values() for iwl in idx2word_list))
         self.logger.info("fitting count vectorizer")
         # this is only for training
-        self.fit_count_vectorizers(
-            span1_list, span2_list, doc_list, graph_list, idx2word_list
-        )
+        if is_train:
+            self.fit_count_vectorizers(
+                span1_list,
+                span2_list,
+                doc_list,
+                graph_list,
+                idx2word_list,
+                edge2dep_list,
+            )
 
         self.logger.info("creating features for train set")
         x = self.features_from_instance(
-            span1_list, span2_list, doc_list, graph_list, idx2word_list
+            span1_list, span2_list, doc_list, graph_list, idx2word_list, edge2dep_list
         )
 
         self.logger.debug("labels: " + str(self.labels))
@@ -207,15 +212,20 @@ class SlotClassifier:
                     (
                         x,
                         y,
-                        arc if arc in self.plausible_relations[x.span_type, y.span_type] else "Nothing",
+                        arc
+                        if arc in self.plausible_relations[x.span_type, y.span_type]
+                        else "Nothing",
                     )
                 )
         return span_pairs
 
-    def preprocess_text(self, text: str) -> Tuple[Doc, nx.Graph, Dict[int, str]]:
+    def preprocess_text(
+        self, text: str
+    ) -> Tuple[Doc, nx.Graph, Dict[int, str], Dict[Tuple[int, int], str]]:
         doc = nlp(text)
         edges = []
         idx2word = {}
+        edge2dep = {}
         # using the tokens themselves would require different instances of the
         # same token to compare? hash? to the same value, but different tokens
         # in the same sentence, that happen to have the same term need to hash?
@@ -226,11 +236,13 @@ class SlotClassifier:
         for token in doc:
             edges.append((token.idx, token.head.idx))
             idx2word[token.idx] = token.text
+            edge2dep[(token.idx, token.head.idx)] = token.dep_
+            edge2dep[(token.head.idx, token.idx)] = token.dep_
         graph = nx.Graph(edges)
         # print(edges)
         # print(graph)
         # print("---")
-        return doc, graph, idx2word
+        return doc, graph, idx2word, edge2dep
 
     def fit_count_vectorizers(
         self,
@@ -239,10 +251,11 @@ class SlotClassifier:
         doc_list: List[Doc],
         graph_list: List[nx.Graph],
         idx2word_list: List[Dict[int, str]],
+        edge2dep_list: List[Dict[Tuple[int, int], str]],
     ) -> None:
 
-        _, shortest_path_list = self.get_shortest_path_features(
-            doc_list, graph_list, span1_list, span2_list, idx2word_list
+        _, shortest_path_list, path_list = self.get_shortest_path_features(
+            doc_list, graph_list, span1_list, span2_list, idx2word_list, edge2dep_list
         )
 
         self.cv_text = CountVectorizer()
@@ -257,6 +270,9 @@ class SlotClassifier:
         # self.logger.debug(span_label_list)
         self.cv_labels.fit(span_label_list)
 
+        self.cv_deps_words = CountVectorizer(ngram_range=(2, 2))  # type: ignore
+        self.cv_deps_words.fit(path_list)
+
     def get_shortest_path_features(
         self,
         doc_list: List[Doc],
@@ -264,7 +280,8 @@ class SlotClassifier:
         span1_list: List[Span],
         span2_list: List[Span],
         idx2word_list: List[Dict[int, str]],
-    ) -> Tuple[np.ndarray, List[str]]:
+        edge2dep_list: List[Dict[Tuple[int, int], str]],
+    ) -> Tuple[np.ndarray, List[str], np.ndarray]:
         sp1_words = [
             self.find_node(doc, span1) for doc, span1 in zip(doc_list, span1_list)
         ]
@@ -288,7 +305,27 @@ class SlotClassifier:
             )
             shortest_path_list.append(shortest_path)
 
-        return f_shortest_path, shortest_path_list
+        f_path_deps = np.array(
+            [
+                self.edge_words_deps(
+                    graph,
+                    self.find_node(doc, sp1),
+                    self.find_node(doc, sp2),
+                    idx2word,
+                    edge2dep,
+                )
+                for graph, sp1, sp2, idx2word, edge2dep, doc in zip(
+                    graph_list,
+                    span1_list,
+                    span2_list,
+                    idx2word_list,
+                    edge2dep_list,
+                    doc_list,
+                )
+            ]
+        )
+
+        return f_shortest_path, shortest_path_list, f_path_deps
 
     def features_from_instance(
         self,
@@ -297,7 +334,8 @@ class SlotClassifier:
         doc_list: List[Doc],
         graph_list: List[nx.Graph],
         idx2word_list: List[Dict[int, str]],
-    ) -> scipy.sparse.spmatrix:
+        edge2dep_list: List[Dict[Tuple[int, int], str]],
+    ) -> spmatrix:
         # spacy words
 
         f_sp1_text = self.cv_text.transform([span1.text for span1 in span1_list])
@@ -310,12 +348,21 @@ class SlotClassifier:
             [span2.span_type.name for span2 in span2_list]
         )
 
-        f_shortest_path, shortest_path_list = self.get_shortest_path_features(
-            doc_list, graph_list, span1_list, span2_list, idx2word_list
+        shortest_path_features = self.get_shortest_path_features(
+            doc_list,
+            graph_list,
+            span1_list,
+            span2_list,
+            idx2word_list,
+            edge2dep_list,
         )
+
+        f_shortest_path, shortest_path_list, f_path_deps = shortest_path_features
 
         f_shortest_path = f_shortest_path.reshape(f_shortest_path.shape[0], 1)
         f_shortest_path_words = self.cv_text.transform(shortest_path_list)
+
+        f_path_deps = self.cv_deps_words.transform(f_path_deps)  # type: ignore
 
         f_sequence_distance = np.array(
             [
@@ -335,6 +382,7 @@ class SlotClassifier:
         self.logger.debug(f_shortest_path.shape)
         self.logger.debug(f_sequence_distance.shape)
         self.logger.debug(f_shortest_path_words.shape)
+        self.logger.debug(f_path_deps.shape)
 
         X_feats = hstack(
             [
@@ -345,16 +393,29 @@ class SlotClassifier:
                 csr_matrix(f_shortest_path),
                 csr_matrix(f_sequence_distance),
                 f_shortest_path_words,
+                csr_matrix(f_path_deps),
             ]
         )
 
         return X_feats
 
     def save_model(self, filename: str) -> None:
-        joblib.dump([self.cls, self.cv_text, self.cv_labels, self.labels], filename)
+        joblib.dump(
+            [
+                self.cls,
+                self.cv_text,
+                self.cv_labels,
+                self.cv_deps_words,
+                self.labels,
+            ],
+            filename,
+        )
 
     def load_model(self, filename: str) -> None:
-        self.cls, self.cv_text, self.cv_labels, self.labels = joblib.load(filename)
+        jll = joblib.load(
+            filename
+        )
+        self.cls, self.cv_text, self.cv_labels, self.cv_deps_words, self.labels = jll
 
     @staticmethod
     def find_node(doc: Doc, span: Span) -> List[spacy.tokens.Token]:
@@ -393,13 +454,45 @@ class SlotClassifier:
         except nx.NetworkXNoPath:
             return -1  # 10000 # float("inf")
 
-    @staticmethod
+    def edge_words_deps(
+        self,
+        graph: nx.Graph,
+        tokens1: List[spacy.tokens.Token],
+        tokens2: List[spacy.tokens.Token],
+        idx2word: Dict[int, str],
+        edge2dep: Dict[Tuple[int, int], str],
+    ) -> str:
+        shortest = self.get_shortest_path(graph, tokens1, tokens2)
+        if shortest is None:
+            return ""
+        one = iter(shortest)
+        two = iter(shortest)
+        next(two)
+        words_deps = [idx2word[shortest[0]]]
+        for left, right in zip(one, two):
+            words_deps.append(edge2dep[left, right])
+            words_deps.append(idx2word[right])
+
+        return " ".join(words_deps)
+
     def edge_words(
+        self,
         graph: nx.Graph,
         tokens1: List[spacy.tokens.Token],
         tokens2: List[spacy.tokens.Token],
         idx2word: Dict[int, str],
     ) -> List[str]:
+        shortest = self.get_shortest_path(graph, tokens1, tokens2)
+        if shortest is None:
+            return []
+        return [idx2word[idx] for idx in shortest]
+
+    @staticmethod
+    def get_shortest_path(
+        graph: nx.Graph,
+        tokens1: List[spacy.tokens.Token],
+        tokens2: List[spacy.tokens.Token],
+    ) -> Optional[List[int]]:
         shortest = None
         for token1 in tokens1:
             for token2 in tokens2:
@@ -409,6 +502,4 @@ class SlotClassifier:
                         shortest = this
                 except nx.NetworkXNoPath:
                     continue
-        if shortest is None:
-            return []
-        return [idx2word[idx] for idx in shortest]
+        return shortest  # type: ignore
