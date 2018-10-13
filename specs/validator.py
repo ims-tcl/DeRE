005596a8ca48104c54b94f4e4d9f9732d8f9b344
Laura Bostan <sarnthil@gmail.com>
2018-10-13 23:05:22 +0200

    Blackify validator, added if __name__ == "__main__"

--- specs/validator.py
+++ specs/validator.py
@@ -1,6 +1,6 @@
-'''
+"""
 This script checks all xml files in the specs folder against the dere.dtd file.
-'''
+"""
 
 import re
 import os.path
@@ -8,37 +8,40 @@ import codecs
 from lxml import etree
 from typing import Optional
 
-dtdRe = re.compile('.*<!DOCTYPE .* ["\'](.*\.dtd)["\']>.*')
-theDtd = '../data/frame-specifications/xmlv1/dere.dtd'
+dtdRe = re.compile(".*<!DOCTYPE .* [\"'](.*\.dtd)[\"']>.*")
+theDtd = "../data/frame-specifications/xmlv1/dere.dtd"
 
-def validate_all()->None:
+
+def validate_all() -> None:
     for inFile in os.listdir():
-        if inFile.split('.')[1]=='xml':
-            print("-"*80)
+        if inFile.split(".")[1] == "xml":
+            print("-" * 80)
             print("Checking " + inFile)
             fdir = os.path.abspath(os.path.dirname(inFile))
-            with codecs.open(inFile, 'r', 'utf-8') as inf:
+            with codecs.open(inFile, "r", "utf-8") as inf:
                 for ln in inf:
                     mtch = dtdRe.match(ln)
                     if mtch:
                         if os.path.isabs(mtch.group(1)):
                             theDtd = mtch.group(1)
                         else:
-                            theDtd = os.path.abspath(fdir + '/' + mtch.group(1))
+                            theDtd = os.path.abspath(fdir + "/" + mtch.group(1))
                         break
 
-            print('Using DTD:', theDtd)
+            print("Using DTD:", theDtd)
 
             parser = etree.XMLParser(dtd_validation=True)
             dtd = etree.DTD(open(theDtd))
             tree = etree.parse(inFile)
 
             valid = dtd.validate(tree)
-            if (valid):
+            if valid:
                 print("XML was valid!")
 
             else:
                 print("XML was not valid:")
                 print(dtd.error_log.filter_from_errors())
 
-validate_all()
+
+if __name__ == "__main__":
+    validate_all()
