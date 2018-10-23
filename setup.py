74b9379435a99693c40024d68f42efaca81fb369
Laura Bostan <sarnthil@gmail.com>
2018-10-23 23:19:42 +0200

    Move test requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,35 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 setup(
-    name='dere',
-    version='0.1',
-    description='Declarative Relation Extraction',
-    url='http://www.ims.uni-stuttgart.de/forschung/ressourcen/werkzeuge/DeRE.en.html',
-    author='Heike Adel, Laura Bostan, Sean Papay, Roman Klinger, Sebastian Pad\u00f3',
+    name="dere",
+    version="0.1",
+    description="Declarative Relation Extraction",
+    url="http://www.ims.uni-stuttgart.de/forschung/ressourcen/werkzeuge/DeRE.en.html",
+    author="Heike Adel, Laura Bostan, Sean Papay, Roman Klinger, Sebastian Pad\u00f3",
     author_email="dereproject@ims.uni-stuttgart.de",
-    license='Apache 2',
-    python_requires='>= 3.7', 
+    license="Apache 2",
+    python_requires=">= 3.7", 
     packages=find_packages(),
     install_requires=[
-        'mypy == 0.620',
-        'mypy-extensions == 0.4.1',
-        'click == 6.7',
-        'numpy >= 1.14.5',
-        'scipy == 1.1.0',
-        'scikit-learn == 0.19.2',
-        'sklearn-crfsuite == 0.3.6',
-        'nltk == 3.3',
-        'networkx >= 2.2',
-        'spacy == 2.0.12',
+        "mypy == 0.620",
+        "mypy-extensions == 0.4.1",
+        "click == 6.7",
+        "numpy >= 1.14.5",
+        "scipy == 1.1.0",
+        "scikit-learn == 0.19.2",
+        "sklearn-crfsuite == 0.3.6",
+        "nltk == 3.3",
+        "networkx >= 2.2",
+        "spacy == 2.0.12",
     ],
+    tests_require = [
+        "pycodestyle",
+        "mypy",
+        "git+https://github.com/numpy/numpy-stubs.git",
+    ]
     entry_points={
-        'console_scripts': [
-            'dere = dere.__main__:cli'
+        "console_scripts": [
+            "dere = dere.__main__:cli"
         ]
     },
     zip_safe=False,
